04361b2c1e73291fcfded4e6ec9ccb1d66a12a72
Daniela Cubreacov <1daniela.cubreacov@gmail.com>
2022-01-27 12:33:32 -0800

    changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 //alert("test");
 
+
   //https://tinyurl.com/dynamic-html-checker
   document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
 
@@ -15,3 +16,28 @@
       }
   } 
 
+  
+    const spans =  document.querySelectorAll('div#play span')
+       console.log(spans);
+
+    for(const mySpan of spans){
+        mySpan.addEventListener('click',function(ev){
+        //alert(mySpan.dataset.actor);
+        highlightActor(mySpan.dataset.actor);
+     });
+    }
+
+    function highlightActor(myActor){
+
+
+        for(const mySpan of spans){
+
+            if (myActor ==mySpan.dataset.actor){
+            mySpan.style.backgroundColor = 'yellow';
+
+        }else{
+            mySpan.style.backgroundColor = 'white';
+            }
+        }
+
+    } 
